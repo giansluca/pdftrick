e12afcc4f223ab02dae67c2aa558951cd7cf8bc4
gians <giansluca@gmail.com>
2020-11-29 14:52:47 +0100

    update travis configuration

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@ if [ $TRAVIS_OS_NAME = 'osx' ]; then
   export SSHPASS=$DEPLOY_PASS
   sshpass -e scp \
   -o stricthostkeychecking=no \
-  target/pdftrick_1.3.1.dmg \
+  $MAC_FILE_PATH \
   $DEPLOY_USER@$DEPLOY_HOST:/home/gians/test-deploy
 else
   pscp \
   -pw $DEPLOY_PASS \
   -hostkey $SERVER_FINGERPRINT \
   -P 22 \
-  target/PdfTrick_1.3.1.exe \
+  $WIN_FILE_PATH \
   $DEPLOY_USER@$DEPLOY_HOST:/home/gians/test-deploy
 fi
 
